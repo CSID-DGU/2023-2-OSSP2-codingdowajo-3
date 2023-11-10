13bdb8dd67e71fb2de5aaffd146f0b58ff29d3c5
Park HyoungJun <parkhj000720@gmail.com>
2023-11-10 17:54:25 +0900

    BlackHisotryPaper and shopPoint

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,10 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager I;
 
+    //User정보
+    public static int ShopPoint = 0;// 상점 재화
+    public static int BlackHistoryPaper = 0;// 흑역사 종이: '회고' 입장권
+
     void Awake()
     {
         I = this;
