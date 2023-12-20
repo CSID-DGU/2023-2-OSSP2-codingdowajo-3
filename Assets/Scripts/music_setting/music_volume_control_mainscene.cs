00413ddb0c84ba0b340f731a130f2fe419fcf683
awrgawa <bartas21@naver.com>
2023-12-20 11:55:42 +0900

    feat: music volume fixed

--- Assets/Scripts/music_setting/music_volume_control_mainscene.cs
+++ Assets/Scripts/music_setting/music_volume_control_mainscene.cs
@@ -7,16 +7,24 @@ public class music_volume_control_mainscene : MonoBehaviour
 {
     public Slider volumeSlider;
     public AudioSource audioSource;
-    public static float volume_sharing; //´Ù¸¥ ¾À¿¡¼­µµ ÀÌ º¼·ý´ë·Î À½¾Ç Ãâ·Â
+    public static float volume_sharing = -1; //´Ù¸¥ ¾À¿¡¼­µµ ÀÌ º¼·ý´ë·Î À½¾Ç Ãâ·Â
 
     // Start is called before the first frame update
     void Start()
     {
-         if (audioSource != null && volumeSlider != null)
-        {
-            audioSource.volume = volumeSlider.value;
-            volume_sharing = audioSource.volume;
-        }
+         if (audioSource != null && volumeSlider != null) {
+            
+            if(volume_sharing != -1) {
+                /* When the volume is not set to its initial value. which means MODIFIED */
+                audioSource.volume = volume_sharing;
+                volumeSlider.value = audioSource.volume;
+            }
+            else {
+                /* Initial Case, set volume to the initial value of the slider */
+                audioSource.volume = volumeSlider.value;
+                volume_sharing = audioSource.volume;
+            }
+         }
     }
     public void OnVolumeChanged()
     {
